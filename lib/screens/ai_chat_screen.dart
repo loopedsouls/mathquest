55ba067c55d8156d7985e346d0393f027f5e8cf1
Luan Nery <79871298+lu4nn3ry@users.noreply.github.com>
2025-09-04 12:36:36 -0400

    feat: Add flutter_markdown dependency and import in AIChatScreen

--- lib/screens/ai_chat_screen.dart
+++ lib/screens/ai_chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import '../theme/app_theme.dart';
 import '../services/ia_service.dart';
 

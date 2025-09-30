3869b0af6ce37551cd36fd5d382b1b3a074f1c88
lu4nn3ry <lu4nn3ry@gmail.com>
2025-09-30 03:37:32 +0000

    android push

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,3 +22,9 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+buildscript {
+    dependencies {
+        classpath 'com.google.gms:google-services:4.3.15' // última versão
+    }
+}
+apply plugin: 'com.google.gms.google-services'

8a48f23828f90b42a801424b3e95cb5a3856f2b9
Luan Nery <79871298+lu4nn3ry@users.noreply.github.com>
2025-09-30 06:05:02 -0400

    Remove conflicting FirebaseAuth import and registration from GeneratedPluginRegistrant.swift

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,7 @@ import Foundation
 
 import cloud_firestore
 import file_picker
-<<<<<<< HEAD
-=======
 import firebase_auth
->>>>>>> 428546f5c27c7e20ddd332068842debbc364e512
 import firebase_core
 import path_provider_foundation
 import printing
@@ -20,10 +17,7 @@ import sqflite_darwin
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
-<<<<<<< HEAD
-=======
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
->>>>>>> 428546f5c27c7e20ddd332068842debbc364e512
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
